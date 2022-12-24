a82736ae1588422e1a1d80574fdcfd0603eb6596
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2022-12-24 20:29:30 +0700

    Update remove btn login

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className="flex flex-col justify-between px-10 py-5 mb-10 font-bold lg:flex-row lg:space-x-5">
       <div>
-        <h1 className="text-7xl">Timur's Daily Blog</h1>
+        <h1 className="text-7xl">Daily Blog</h1>
         <h2 className="mt-5 md:mt-0">
           Welcome to{" "}
           <span className="underline decoration-4 decoration-[#f7ab0a]">
@@ -13,10 +13,10 @@ function Banner() {
           favorite blog in the Devosphere
         </h2>
       </div>
-      <p className="max-w-sm mt-5 text-gray-400 md:mt-2">
+      {/* <p className="max-w-sm mt-5 text-gray-400 md:mt-2">
         New product feature | The latest in technology | The weekly debugging
         nightmares & More!
-      </p>
+      </p> */}
     </div>
   );
 }
